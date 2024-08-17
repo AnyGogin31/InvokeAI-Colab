35118975985af3c555a74930fabb3ad506d1455e
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-08-17 23:35:37 +0300

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Creating an environment..."
+conda create -p $INVOKEAI_KERNEL_PREFIX python=3.10 -y
+
+echo "Activation of the environment..."
+eval "$(conda shell.bash hook)"
+conda activate "$INVOKEAI_KERNEL_NAME"
+
+echo "Installing dependencies InvokeAI..."
+pip install InvokeAI --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu121
+
+echo "Installing additional packages..."
+conda install glib=2.51.0 -y
+pip install --upgrade jupyter matplotlib
+pip install opencv-python-headless
+pip install pyngrok
+
+echo "Disconnecting from the environment..."
+conda deactivate
+
+echo "Installation complete."
