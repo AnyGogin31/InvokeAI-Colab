5cec0b01189ab523b1a922d8bd44937e4656667c
AnyGogin31 <148564043+AnyGogin31@users.noreply.github.com>
2024-09-19 09:02:33 +0000

    Adding xfromer dependencies to the installation

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,6 +9,7 @@ conda activate "$INVOKEAI_KERNEL_NAME"
 
 echo "Installing dependencies InvokeAI..."
 pip install InvokeAI --use-pep517 --extra-index-url https://download.pytorch.org/whl/cu121
+pip install "InvokeAI[xformers]" --use-pep517
 
 echo "Installing additional packages..."
 conda install glib=2.51.0 -y
